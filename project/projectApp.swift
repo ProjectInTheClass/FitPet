fe223c138b329d696d4b88b0ec7846a44364df39
백대홍 <bdh3620@naver.com>
2023-02-17 22:29:55 +0900

    first commit

---
+++ project/projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectApp.swift
+//  project
+//
+//  Created by 백대홍 on 2023/02/17.
+//
+
+import SwiftUI
+
+@main
+struct projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
