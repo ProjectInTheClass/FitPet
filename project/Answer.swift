345c0558ec6157152c78377627728e5d69446740
백대홍 <bdh3620@naver.com>
2023-03-11 01:31:52 +0900

    온보딩 페이지 수정 및 자가진단 페이지 사진 추가

---
+++ project/Answer.swift
@@ -0,0 +1,20 @@
+//
+//  Answer.swift
+//  project
+//
+//  Created by 백대홍 on 2023/03/11.
+//
+
+import SwiftUI
+
+struct Answer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Answer_Previews: PreviewProvider {
+    static var previews: some View {
+        Answer()
+    }
+}
---
+++ project/CompletionPage.swift
@@ -0,0 +1,20 @@
+//
+//  CompletionPage.swift
+//  project
+//
+//  Created by 백대홍 on 2023/03/11.
+//
+
+import SwiftUI
+
+struct CompletionPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CompletionPage_Previews: PreviewProvider {
+    static var previews: some View {
+        CompletionPage()
+    }
+}
